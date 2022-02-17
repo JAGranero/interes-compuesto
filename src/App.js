2137d101da414c41b4b6d93e96e8e1ad758ccc69
JAGranero <43791192+JAGranero@users.noreply.github.com>
2022-02-17 19:22:02 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import "./styles.css";
+import styled from "styled-components";
+import { Formik, Form } from "formik";
+import Input from "./components/Input";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  align-items: center;
+`;
+
+const Section = styled.section`
+  background-color: #eee;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+  width: 500px;
+  box-shadow: 0px 2px 3px rgb(0, 0, 0, 0.3);
+`;
 
 export default function App() {
+  const handleSubmint = () => {};
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Container>
+      <Section>
+        <Formik
+          initialValues={{ deposit: "", contribution: "", years: "", rate: "" }}
+          onSubmit={handleSubmint}
+        >
+          <Form>
+            <Input name="deposit" label="Deposito inicial" />
+          </Form>
+        </Formik>
+      </Section>
+    </Container>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,8 @@
+import style from "styled-components";
+import { useField } from "formik";
+
+const Input = ({ label, ...props }) => {
+  return "lala";
+};
+
+export default Input;
