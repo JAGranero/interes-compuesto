6ef51e777b234c16a47aed0d760cdb41b867a222
JAGranero <43791192+JAGranero@users.noreply.github.com>
2022-02-17 19:49:26 -0300

    push de INputs a medio hacer

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,31 @@
-import style from "styled-components";
+import styled from "styled-components";
 import { useField } from "formik";
 
+const Control = styled.div`
+  margin-botton: 20px;
+`;
+
+const Label = styled.label`
+  color: #000;
+  display: block;
+  margin-bottom: 5px;
+`;
+
+const MyInput = styled.input`
+  outline: none;
+  padding: 8px;
+  border: solid 1px #b1b3b5;
+  border-radius: 4px;
+  width: 100%;
+  margin-bottom: 5px;
+`;
+
 const Input = ({ label, ...props }) => {
-  return "lala";
+  return (
+    <Control>
+      <Label> {label} </Label>
+    </Control>
+  );
 };
 
 export default Input;
